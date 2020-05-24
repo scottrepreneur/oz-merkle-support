73d1228d15bd7bb34c09364cedd8e77aff6e4dfc
scottrepreneur <me@stwoh.com>
2020-05-24 08:06:03 -0500

    working

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -3,19 +3,19 @@ pragma solidity 0.6.8;
 
 import "@openzeppelin/contracts/cryptography/MerkleProof.sol";
 
+
 contract MyContract {
 
   /// Libraries
   using MerkleProof for bytes32[];
 
-  constructor()
-    public
-  {
+  constructor() public {
 
   }
-  function claim(bytes32[] memory proof, bytes32 root) public view returns (bool) {
-    bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
-    require(proof.verify(root, leaf), "MerkleProof not working");
+
+
+  function claim(bytes32[] memory proof, bytes32 root, bytes32 leaf) public view returns (bool) {
+    require(MerkleProof.verify(proof, root, leaf), "MerkleProof not working");
     return true;
   }
 }
--- test/MyContract.test.js
+++ test/MyContract.test.js
@@ -1,15 +1,19 @@
 const { keccakFromString, bufferToHex } = require('ethereumjs-util');
 const {ethers} = require('ethers')
 let catchRevert = require("./exceptionsHelpers.js").catchRevert
-const { MerkleTree } = require('./merkleTree.js')
-var MyContract= artifacts.require('./MyContract')
+const MerkleTree = require('./merkleTree.js').MerkleTree
+// const { accounts, contract, web3 } = require('@openzeppelin/test-environment');
+var MyContract = artifacts.require('./MyContract')
+const { keccakFromString } = require('ethereumjs-util')
+
+// const MerkleProofWrapper = artifacts.require('MerkleProofWrapper');
 
 contract('MyContract', function(accounts) {
 
   const owner = accounts[0]
   const random = accounts[1]
-  const redeemer = accounts[2]
-  const addresses = [owner, random, redeemer]
+  const redeemer = 'c'
+  const addresses = ['a', 'b', 'c', 'd']
 
   const merkleTree = new MerkleTree(addresses);
   const root = merkleTree.getHexRoot();
@@ -17,11 +21,9 @@ contract('MyContract', function(accounts) {
   const proof = merkleTree.getHexProof(redeemer);
   //console.log(proof)
 
-  let instance
-
   // Before Each
   beforeEach(async () => {
-    instance = await MyContract.new()
+    this.merkleProof = await MyContract.new()
   })
 
   describe("Functions", () => {
@@ -29,7 +31,8 @@ contract('MyContract', function(accounts) {
     describe("claim()", async () => {
 
       it("check redeemer account via MerkleProof verify()", async () => {
-        const result = await instance.claim(proof, root, {from:redeemer})
+        const leaf = keccakFromString('c');
+        const result = await this.merkleProof.claim(proof, root, leaf)
         assert.isTrue(result, "MerkleProof not working")
       })
     })
